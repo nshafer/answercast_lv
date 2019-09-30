cf8fd1e19781b35de1e0406465ca04e4cc2c113a
Nathan Shafer <nate@lotech.org>
2019-09-30 14:13:56 -0700

    Run on port 8000 in prod

--- config/prod.exs
+++ config/prod.exs
@@ -10,6 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :answercast, AnswercastWeb.Endpoint,
+  http: [port: 8000],
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
