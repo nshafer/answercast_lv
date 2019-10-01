fe61772b9bc4c125bab348952b0eb8b3ec5d0019
Nathan Shafer <nate@lotech.org>
2019-10-01 09:10:06 -0700

    Show debug logs in prod for now

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,8 @@ config :answercast, AnswercastWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :info
+#config :logger, level: :info
+config :logger, level: :debug
 
 # ## SSL Support
 #
